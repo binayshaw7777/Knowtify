c3657ec7cafd87eaef89aa16238d7b44d7398800
binay shaw <binayshaw7777@gmail.com>
2024-05-09 13:17:46 +0530

    Added Settings items: TODO: Func + UI

---
+++ composeApp/src/commonMain/kotlin/presentation/component/SettingItem.kt
@@ -0,0 +1,44 @@
+package presentation.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun SettingItem(
+    onClick: () -> Unit,
+    resource: DrawableResource,
+    itemName: String
+) {
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            modifier = Modifier.size(24.dp),
+            painter = painterResource(resource = resource),
+            contentDescription = null,
+            tint = Color.Unspecified,
+        )
+        Text(
+            text = itemName
+        )
+    }
+}
--- composeApp/src/commonMain/kotlin/presentation/setting/Setting.kt
+++ composeApp/src/commonMain/kotlin/presentation/setting/Setting.kt
@@ -1,17 +1,35 @@
 package presentation.setting
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import co.touchlab.kermit.Logger
+import knowtify.composeapp.generated.resources.Res
+import knowtify.composeapp.generated.resources.about
+import knowtify.composeapp.generated.resources.compose_multiplatform
+import knowtify.composeapp.generated.resources.general
+import knowtify.composeapp.generated.resources.invite_others
+import knowtify.composeapp.generated.resources.licenses
+import knowtify.composeapp.generated.resources.theme
 import navigation.LocalNavHost
+import org.jetbrains.compose.resources.stringResource
+import presentation.component.SettingItem
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,7 +49,54 @@ fun Setting() {
                 }
             )
         }
-    ) {
+    ) { paddingValues ->
 
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(horizontal = 16.dp)
+        ) {
+
+            LazyColumn {
+                item {
+                    Box(modifier = Modifier.padding(16.dp)) {
+                        Text(
+                            text = stringResource(Res.string.general),
+                            style = MaterialTheme.typography.bodyMedium,
+                            fontWeight = FontWeight.Medium
+                        )
+                    }
+                }
+                item {
+                    SettingItem(
+                        onClick = { Logger.d("Clicked on theme") },
+                        resource = Res.drawable.compose_multiplatform,
+                        itemName = stringResource(Res.string.theme)
+                    )
+                }
+                item {
+                    SettingItem(
+                        onClick = { Logger.d("Clicked on theme") },
+                        resource = Res.drawable.compose_multiplatform,
+                        itemName = stringResource(Res.string.about)
+                    )
+                }
+                item {
+                    SettingItem(
+                        onClick = { Logger.d("Clicked on theme") },
+                        resource = Res.drawable.compose_multiplatform,
+                        itemName = stringResource(Res.string.invite_others)
+                    )
+                }
+                item {
+                    SettingItem(
+                        onClick = { Logger.d("Clicked on theme") },
+                        resource = Res.drawable.compose_multiplatform,
+                        itemName = stringResource(Res.string.licenses)
+                    )
+                }
+            }
+        }
     }
 }
